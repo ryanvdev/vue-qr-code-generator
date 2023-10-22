6d3de6cee520bda0a5948515ab92b27977b61437
tuannguyen <tuannguyen7326.dev@gmail.com>
2023-10-22 16:01:11 +0700

    Fix before building

--- landing_page/src/services/posts_service/posts_service.ts
+++ landing_page/src/services/posts_service/posts_service.ts
@@ -2,7 +2,7 @@ import { postsFetcher } from './posts_fetcher';
 import { marked } from 'marked';
 
 export const fetchPost = async (name: string) => {
-    const res = await postsFetcher.get('/home.md');
+    const res = await postsFetcher.get(`/${name}.md`);
     const data = marked.parse(res.data);
     return data;
 };
